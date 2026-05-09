ae2a7145b69fd0b4a3742f1b9a0ebf8e155af11e
DeHampster <henry.retzer@gmail.com>
2026-05-09 16:39:22 -0500

    Added scoring and multipliers

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
@@ -11,10 +12,22 @@ public class GameManager : MonoBehaviour
 
     public static GameManager instance;
 
+    public int currentScore;
+    public int scorePerNote = 100;
+    public int currentMultiplier;
+    public int multiplierTracker;
+    public int[] multiplierThresholds;
+
+    public Text scoreText;
+    public Text multiText;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         instance = this;
+
+        scoreText.text = "Score: 0";
+        currentMultiplier = 1;
     }
 
     // Update is called once per frame
@@ -35,12 +48,31 @@ public class GameManager : MonoBehaviour
     public void NoteHit()
     {
        Debug.Log("hit on time"); 
+
+        if(currentMultiplier - 1 < multiplierThresholds.Length){
+            multiplierTracker++;
+
+            if(multiplierThresholds[currentMultiplier - 1] <= multiplierTracker)
+            {
+                multiplierTracker = 0;
+                currentMultiplier++;
+            }
+        }
+
+        multiText.text = "Multiplier: X" + currentMultiplier;
+
        
+       currentScore += scorePerNote * currentMultiplier;
+       scoreText.text = "Score: " + currentScore;
     }
 
     public void NoteMissed()
     {
         Debug.Log("missed note");
+
+        currentMultiplier = 1;
+        multiplierTracker = 0;
+        multiText.text = "Multiplier: X" + currentMultiplier;
     }
 
 }
